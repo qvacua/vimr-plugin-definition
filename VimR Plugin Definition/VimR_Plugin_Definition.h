378abe33246b9e20cd057f250b0988bc0dd692e7
Tae Won Ha <qvacua@me.com>
2014-07-20 09:25:04 +0200

    add xcode project

---
+++ VimR Plugin Definition/VimR_Plugin_Definition.h
@@ -0,0 +1,13 @@
+//
+//  VimR_Plugin_Definition.h
+//  VimR Plugin Definition
+//
+//  Created by Tae Won Ha on 20/07/14.
+//  Copyright (c) 2014 Tae Won Ha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VimR_Plugin_Definition : NSObject
+
+@end
---
+++ VimR Plugin DefinitionTests/VimR_Plugin_DefinitionTests.m
@@ -0,0 +1,34 @@
+//
+//  VimR_Plugin_DefinitionTests.m
+//  VimR Plugin DefinitionTests
+//
+//  Created by Tae Won Ha on 20/07/14.
+//  Copyright (c) 2014 Tae Won Ha. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VimR_Plugin_DefinitionTests : XCTestCase
+
+@end
+
+@implementation VimR_Plugin_DefinitionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
